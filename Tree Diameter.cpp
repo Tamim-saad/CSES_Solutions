0686a2c27e6c67170dabe86b1c254ba516dbf679
Tamim Hasan Saad <tamimsaad1812@gmail.com>
2024-07-03 11:43:58 +0600

    add tree diameter.cpp

---
+++ Tree Diameter.cpp
@@ -0,0 +1,190 @@
+// say Alhamdulillah
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast                                                                   \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+#define ll long long
+#define init                                                                   \
+  ll count = 0, zz, t;                                                         \
+  cin >> t;                                                                    \
+  while (t--)
+#define ld long double
+#define ed "\n"
+#define eb emplace_back
+#define con continue
+#define bre break
+#define pl " "
+#define pll " --- "
+// #define size size()
+#define mpr make_pair
+#define vec vector<ll>
+#define lmax LONG_LONG_MAX
+#define lmin LONG_LONG_MIN
+#define forr(i, a, b) for (ll i = a; i < b; i++)
+#define forrev(i, a, b) for (ll i = a; i >= b; i--)
+
+// #define v.all v.begin(), v.end()
+
+#define printArrWhole(arr, n)                                                  \
+  for (ll i = 0; i < n; i++)                                                   \
+    cout << arr[i] << " ";                                                     \
+  cout << endl;
+
+#define printArrLim(arr, in, n)                                                \
+  for (ll i = in; i <= n; i++)                                                 \
+    cout << arr[i] << " ";                                                     \
+  cout << endl;
+
+#define printYesNo(flag)                                                       \
+  if (flag == 0)                                                               \
+    cout << "NO";                                                              \
+  else                                                                         \
+    cout << "YES";                                                             \
+  cout << endl;
+
+#define printvcWhole(vc)                                                       \
+  for (auto x : vc)                                                            \
+    cout << x << " ";                                                          \
+  cout << endl;
+
+#define printvcLim(vc, in, n)                                                  \
+  for (ll i = in; i <= n; i++)                                                 \
+    cout << vc[i] << " ";                                                      \
+  cout << endl;
+
+#define printdqWhole(dq)                                                       \
+  for (auto x : dq)                                                            \
+    cout << x << " ";                                                          \
+  cout << endl;
+
+#define printdqLim(dq, in, n)                                                  \
+  for (ll i = in; i <= n; i++)                                                 \
+    cout << dq[i] << " ";                                                      \
+  cout << endl;
+
+#define sortArr(arr, n) sort(arr, arr + n);
+#define sortvc(v) sort(v.begin(), v.end());
+#define rev(v) reverse(v.begin(), v.end());
+// #define find(v, x) find(v.begin(), v.end(), x);
+
+const ll mod = 1e9 + 7;
+
+vector<vector<ll>> adj_list;
+vector<ll> par, vis;
+ll node = -1, depth = 0, maxdepth = 0;
+
+void dfs(ll curr, ll parent = -1) {
+  if (vis[curr] != -1) {
+    if (vis[curr] == 1)
+      depth--;
+    return;
+  }
+
+  par[curr] = parent;
+  vis[curr] = 0;
+  depth++;
+  if (maxdepth <= depth) {
+    maxdepth = depth;
+    node = curr;
+  }
+  // cout << curr << pl << depth << ed;
+
+  for (auto x : adj_list[curr]) {
+    if (par[curr] != x)
+      dfs(x, curr);
+  }
+
+  vis[curr] = 1;
+  depth--;
+  return;
+}
+
+int main() {
+  fast
+  // init
+  {
+    // count++;
+    ll i, j, k, x, y, z, p, q, m, n, l, r, t, l1, r1, ans, pos, flag, sum;
+    ll mini = lmax, maxi = -1, piv, temp, low, high, rem, prev, now, mid, bit;
+    ll dif = 0, change = 0, total = 0, same = 0, plus = 0, minus = 0, zero = 0;
+
+    cin >> n;
+
+    // cout<<piv;
+
+    // char ch, ch1, ch2, ch3, ch4;
+    // char charr[n][n];
+    // string sarr[n];
+    // string s, s1 = "", s2 = "";
+    // string res1 = "", res2 = "", chk1 = "", chk2 = "", chk3 = "", chk4 = "",
+    // pivs = "", rems = "";
+
+    // n = s.size;
+    // ll arr[n+1], number[n], cost[n], forsum[n+1], backsum[n];
+    //
+    // vector<ll> v, v1, v2, vans, vpos, vlen, vsum, vdif;
+    // vector<ll> par(n), v1(n), v2(n), vans;
+    // vector<vec> dp(n+1,vec(x+1,0));
+    // vector<tuple<ll,ll,ll>> vtup, vtup1, vtup2, vtup3;
+    // vector <string> vstr(n);
+    // vector<pair<ll, ll>> vpr, vpr1, vpr2, vpr3;
+    // vector<vec> vdarr(n, vec(10, 0));
+    // deque<ll> dq, dq1, dq2;
+
+    // pair<ll, ll> pr, pr1, pr2,pr3, pr4;
+    // set <ll> st, st1, st2, ms, ldiff, rdiff;
+    // multiset <pair<ll,ll>> mst, mst1, mstans;
+    // multiset<ll, greater<ll>> mst, mst1, mst2;
+    // priority_queue<ll> pq, pq1, pq2;
+    // stack<pair<ll,ll>> stck, stck1, stck2;
+    // map<ll, ll> mp, mp1, mp2;
+    // map<pair<ll,ll>,ll> mpair, mpair1, mpair2;
+    // vector<pair<ll,ll>> vpr, vpr1, vpr2;
+    // queue<ll> vq[10],vq1,vq2;
+
+    // multiset<ll>::iterator itr, itr1, itr2;
+    // Point pa,pb,pc,pd;//,pt[1001];
+    // Node pa,pb,pt[n];
+    // cout<<"-----------------yess"<<endl;
+
+    // ans=LONG_LONG_MAX;
+    // maxi = 0;
+    // t = 0;
+
+    adj_list.resize(n + 1);
+    par.resize(n + 1, -1);
+    vis.resize(n + 1, -1);
+
+    forr(i, 1, n) {
+      cin >> x >> y;
+      adj_list[x].eb(y);
+      adj_list[y].eb(x);
+    }
+
+    dfs(1);
+    now = node;
+
+    fill(par.begin(), par.end(), -1);
+    fill(vis.begin(), vis.end(), -1);
+    node = -1, depth = 0, maxdepth = 0;
+
+    dfs(now);
+
+    vec path;
+    while (node!= now) {
+      path.eb(node);
+      node = par[node];
+    }
+    ans=path.size();
+    path.eb(now);
+
+    cout<<ans<<ed;
+    // printvcWhole(path);
+
+    // cout<<ans<<pl;
+  }
+}
